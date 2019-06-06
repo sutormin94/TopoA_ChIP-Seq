f11c60193f8c554ebb039a40066cb3fccaa68505
sutormin94 <sutormin94@users.noreply.github.com>
2019-06-06 17:05:52 +0300

    MC

--- Bed_to_wig_converter.py
+++ Bed_to_wig_converter.py
@@ -11,22 +11,31 @@
 
 
 #Path to the input file
-filein_path="Path\file.bed"
+filein_path="F:\V_cholerae_ToxR\Cov_depth_nodup\SRR2188532_1_name_sorted_fm_ps_nd.bed"
 #Path to the output file.
-fileout_path="Path\file.wig"
+fileout_path="F:\V_cholerae_ToxR\Cov_depth_nodup\ToxR_ChIP_control_nodup.wig"
 #ID or short description of the track (will be the name of a track in IGV).
-name=''
+name='ToxR_ChIP_control_nodup'
 #ID of chromosome (for w3110_Mu_SGS: NC_007779.1_w3110_Mu)
 Chromosome_name=''
+#Mode for Chromosome name writing: 0 - auto detection from bed file provided, 1 - manualy provided by user in Chromosome_name variable.
+Auto_or_manual=int(0)
 
 filein=open(filein_path, 'r')
 fileout=open(fileout_path, 'w')
 
-fileout.write('track type=wiggle_0 name="'+name+'" autoScale=off viewLimits=0.0:25.0\r\nfixedStep chrom='+Chromosome_name+' start=1 step=1\r\n')
 
+Ar_of_Cromosome_names=[]
 for line in filein:
     line=line.rstrip().split('\t')
-    fileout.write(line[2]+'\r\n')
+    if line[0] not in Ar_of_Cromosome_names:
+        if Auto_or_manual==0:
+            fileout.write('track type=wiggle_0 name="'+name+'" autoScale=off viewLimits=0.0:25.0\nfixedStep chrom='+str(line[0])+' start=1 step=1\n')
+        elif Auto_or_manual==1:
+            fileout.write('track type=wiggle_0 name="'+name+'" autoScale=off viewLimits=0.0:25.0\nfixedStep chrom='+Chromosome_name+' start=1 step=1\n')
+        Ar_of_Cromosome_names.append(line[0])
+    else:
+        fileout.write(line[2]+'\n')
     
 filein.close()
 fileout.close()
--- Compute_fold_enrichment.py
+++ Compute_fold_enrichment.py
@@ -10,15 +10,15 @@
 
 
 #Path to the file with IP data
-IP_path="F:\E_coli_ChIPs\Cov_depth\SRR1285993_1.wig"
+IP_path="F:\V_cholerae_ToxR\Cov_depth\ToxR_ChIP_B.wig"
 #Path to the file Mock control data
-Mock_path="F:\E_coli_ChIPs\Cov_depth\SRR1285994_1.wig"
+Mock_path="F:\V_cholerae_ToxR\Cov_depth\ToxR_ChIP_control.wig"
 #ID or short description of the track (will be the name of a track in IGV).
-name='RpoS 2'
+name='ToxR_ChIP_B_Fold_enrichment'
 #ID of chromosome (for w3110_Mu_SGS: NC_007779.1_w3110_Mu)
-Chromosome_name='NC_007779.1_w3110_Mu'
+Chromosome_name=''
 #Output path to the final file (fold enrichment).
-FE_file_path="F:\E_coli_ChIPs\Cov_depth\RpoS_2_FE.wig"
+FE_file_path="F:\V_cholerae_ToxR\Cov_depth\ToxR_ChIP_B_Fold_enrichment.wig"
 
 
 #######
@@ -28,13 +28,16 @@ FE_file_path="F:\E_coli_ChIPs\Cov_depth\RpoS_2_FE.wig"
 def wig_parsing(wigfile):
     print('Now is processing: ' + str(wigfile))
     wigin=open(wigfile, 'r')
-    NE_values=[]
+    Dict_of_chromosomes_data={}
     for line in wigin:
         line=line.rstrip().split(' ')
+        if line[0]=='fixedStep':
+            chrom_name=line[1].split('=')[1]
+            Dict_of_chromosomes_data[chrom_name]=[]
         if line[0] not in ['track', 'fixedStep']:
-            NE_values.append(float(line[0]))
+            Dict_of_chromosomes_data[chrom_name].append(float(line[0]))
     wigin.close()
-    return NE_values
+    return Dict_of_chromosomes_data
     
 IP=wig_parsing(IP_path)
 Mock=wig_parsing(Mock_path)
@@ -42,13 +45,13 @@ Mock=wig_parsing(Mock_path)
 
 FE_out=open(FE_file_path, 'w')
 #Write file with fold enrichment data.
-FE_out.write('track type=wiggle_0 name="'+name+'" autoScale=off viewLimits=0.0:25.0\nfixedStep chrom='+Chromosome_name+' start=1 step=1\n')
-
-for i in range(len(IP)):
-    if Mock[i]!=0:
-        FE_data_position=IP[i]/Mock[i]
-        FE_out.write(str(FE_data_position)+'\n')
-    else:
-        FE_out.write(str(0)+'\n')
+for Chromosome_name, data in IP.items():
+    FE_out.write('track type=wiggle_0 name="'+name+'" autoScale=off viewLimits=0.0:25.0\nfixedStep chrom='+Chromosome_name+' start=1 step=1\n')
+    for i in range(len(data)):
+        if Mock[Chromosome_name][i]!=0:
+            FE_data_position=IP[Chromosome_name][i]/Mock[Chromosome_name][i]
+            FE_out.write(str(FE_data_position)+'\n')
+        else:
+            FE_out.write(str(0)+'\n')
     
 FE_out.close()
